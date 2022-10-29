14c224de98f4506d046c3907a34495fe2320d634
Jayla Young <jaylayoung1@gmail.com>
2022-10-29 09:35:02 -0400

    updated game logic. issues with css p1 p2 button

--- app.js
+++ app.js
@@ -8,22 +8,46 @@ const state = {
     currentQuestion: {}
 }
 
+let questions = []
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // MAIN DOM ELEMENT
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-const question = document.querySelector("#question");
-const a = document.querySelector("#A");
-const b = document.querySelector("#B");
-const c = document.querySelector("#C");
-const d = document.querySelector("#D");
-const p1score = document.querySelector("#Player1 h4");
-const p2score = document.querySelector("#Player2 h4");
+const $question = $("#question");
+const $a = $("#A");
+const $b = $("#B");
+const $c = $("#C");
+const $d = $("#D");
+const p1score = $("#player1 h4");
+const p2score = $("#player2 h4");
 
 console.log(p2score, p1score);
 
-// const URL = "https://cdn.contentful.com/spaces/wppm258xelxu/environments/master/entries?access_token=kXoSt3WXJZUsg8-akhTlSFtc47CUOpEmtaPKs2a_saI"
-//         $.ajax(URL)
-//             .then((data) => {
-//                 console.log(data)
+const URL = "https://cdn.contentful.com/spaces/wppm258xelxu/environments/master/entries?access_token=kXoSt3WXJZUsg8-akhTlSFtc47CUOpEmtaPKs2a_saI"
+$.ajax(URL)
+    .then((data) => {
+        questions = data.items.map((q) => q.fields);
+        console.log(data);
+        console.log(questions);
+    })
 
-//             })
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const setBoard = (q) => {
+    // getting a random question
+    const randomIndex = Math.floor(Math.random() * q.length)
+    cons randomQuestion = q[randomIndex]
+
+    // update question
+    $question.innerText = randomQuestion.question
+    $a.text() = randomQuestion.a
+    $b.text() = randomQuestion.b
+    $c.text() = randomQuestion.c
+    $d.text() = randomQuestion.a
+
+    // update player scores
+    $p1score.text() = state.player1
+    $p2score.text() = state.player1
+
+}
